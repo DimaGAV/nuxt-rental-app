1dc68ef51a9e7516da63ad120e58296ebf951c81
DimaGAV <gav82gav@gmail.com>
2025-02-12 23:05:25 +0300

    start form

--- components/BookingForm.vue
+++ components/BookingForm.vue
@@ -1,24 +1,82 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <input v-model="name" type="text" placeholder="Ваше имя" />
-    <input v-model="email" type="email" placeholder="Email" />
-    <button type="submit">Забронировать</button>
-  </form>
+  <div class="booking">
+    <div class="booking__overlay" @click="closeModal"></div>
+    <div class="booking__modal">
+      <button class="booking__close" @click="closeModal">x</button>
+      <h2 class="booking__title">Забронировать домик</h2>
+      <p class="booking__text">
+        Оставьте свои контакты, мы подберем для вас подходящий домик
+      </p>
+
+      <Form @submit="submitForm" v-slot="{ errors }" class="booking__form">
+        <div class="booking__field">
+          <Field
+            name="name"
+            v-model="form.name"
+            type="text"
+            class="booking__input"
+            placeholder="Ваше имя"
+            rules="required|min:2"
+          />
+          <span class="booking__error" v-if="errors.name">{{
+            errors.name
+          }}</span>
+        </div>
+
+        <div class="booking__field">
+          <Field
+            name="phone"
+            v-model="form.phone"
+            type="tel"
+            class="booking__input"
+            placeholder="+7 (___) ___-__-__"
+            rules="required|phone"
+          />
+          <span class="booking__error" v-if="errors.phone">{{
+            errors.phone
+          }}</span>
+        </div>
+
+        <div class="booking__field">
+          <Field
+            name="comment"
+            v-model="form.comment"
+            as="textarea"
+            class="booking__textarea"
+            placeholder="Комментарий"
+            rules="min:5"
+          />
+        </div>
+
+        <div class="booking__checkbox">
+          <input type="checkbox" id="agree" v-model="form.agree" />
+          <label for="agree"
+            >Я соглашаюсь с <a href="#">Политикой Конфеденциальности</a></label
+          >
+        </div>
+
+        <button
+          type="submit"
+          class="booking__button"
+          :disabled="isSubmitting || !form.agree"
+        >
+          {{ isSubmitting ? "Отправка..." : "Отправить" }}
+        </button>
+        <p
+          v-if="message"
+          :class="[
+            'booking__message',
+            sucsses ? 'booking__messagge--sucsses' : 'booking__message--error',
+          ]"
+        >
+          {{ message }}
+        </p>
+      </Form>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import { useForm } from "vee-validate";
-import * as yup from "yup";
-
-const { defineField, handleSubmit } = useForm({
-  validationSchema: yup.object({
-    name: yup.string().required("Введите имя"),
-    email: yup.string().email("Неверный email").required("Введите email"),
-  }),
-});
-
-const submitForm = handleSubmit((values) => {
-  console.log("Отправка формы", values);
-});
+<script>
+export default {};
 </script>
+<style lang=""></style>
